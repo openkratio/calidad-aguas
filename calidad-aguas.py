8b590489763bb4a160b576b4315f3b1bfabc5189
Christian Lopez Espinola <clopez@emergya.com>
2012-10-16 18:33:13 +0200

    Full range.

--- calidad-aguas.py
+++ calidad-aguas.py
@@ -2,7 +2,7 @@ from calidadaguas import CSVWriter
 from calidadaguas import NayadeScraping
 
 # We don't know the number of beaches, we have to bruteforce it.
-for i in range(1,35):
+for i in range(1, 3095):
   # Get the beach info.
   scraping = NayadeScraping.NayadeScraping()
   scraping.scrap(i)
