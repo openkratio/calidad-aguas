fa2e49cb1c1b4561822e332eef39d305fa42aeca
Manuel Flores <manuelfloresv@gmail.com>
2012-11-19 16:42:27 +0100

    Added support to files, logging and multithreading
    Added config file, save CSVs and log to files, and concurrent http requests.

--- src/nayade.rb
+++ src/nayade.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# nayade.rb versión 0.2 (21/10/2012)
+# nayade.rb versión 0.3 (17/11/2012)
 
 # Scraping al Sistema de Información Nacional de Aguas de Baño (Náyade)
 # para la iniciativa #adoptaunaplaya - http://adoptaunaplaya.es/site/
@@ -8,26 +8,13 @@
 # Autor: @manufloresv (Manuel Flores)
 # Licencia: MIT License
 
-# Uso:
-# ruby nayade.rb [--todas|--ultimas] [<comienzo>] [<final>] > csv.csv
-# --todas     todas las mediciones
-# --ultimas   solo la medición más reciente (opcional, por defecto)
-# <comienzo>  código de la primera playa (1 por defecto)
-# <final>     código de la última playa (constante final por defecto)
-
-# Ejemplo completo:
-# ruby nayade.rb > calidad-aguas.csv
-# ruby nayade.rb --todas > calidad-aguas-historico.csv
-
-# Ejemplo por tramos:
-# ruby nayade.rb 1 500 > calidad-aguas.csv
-# ruby nayade.rb 501 1000 >> calidad-aguas.csv
-# ruby nayade.rb 1001 >> calidad-aguas.csv
+# Uso: ruby nayade.rb
+# Fichero de configuración: config.yml
 
 # Explicación:
 # En Náyade cada zona de baño está numerada con un código entre 1 y 1976.
 # Este script pide las páginas con los datos de localización y muestreos de
-# cada zona de baño, extrae los valores y devuelve un fichero CSV.
+# cada zona de baño, extrae los valores y los guarda en ficheros CSV.
 # He usado expresiones regulares en vez de XPath debido a que en el HTML de
 # la web de Náyade se usa el mismo atributo class para los diferentes datos.
 
@@ -35,9 +22,15 @@ require 'net/http'
 require 'uri'
 require 'cgi'
 require 'csv'
+require 'yaml'
 
 NAYADE_URL = "http://nayade.msc.es/Splayas/ciudadano/ciudadanoVerZonaAction.do"
 MAX_PLAYAS = 1976
+
+@count = 0
+
+class NayadeError < StandardError
+end
 
 class String
   def td_scan(label, prefix="")
@@ -46,19 +39,23 @@ class String
   end
 end
 
-def playa(cod, historico)
-  begin
-    $stderr.print cod # imprimir cod actual en stderr
+def playa(cod)
+  ultimas = ""
+  todas = ""
+  log = ""
 
+  begin
     localizacion = Net::HTTP.get URI.parse(NAYADE_URL + "?codZona=#{cod}")
     localizacion.encode!("UTF-8","ISO-8859-1")
 
     cpmn = ["Comunidad Autónoma", "Provincia", "Municipio", "Zona Agua Baño"]
     comunidad, provincia, municipio, nombre = cpmn.map do |i|
-      localizacion.td_scan(i).first.first
+      v = localizacion.td_scan(i)
+      raise NayadeError, "Aquí no hay playa, vaya vaya." if v.empty?
+      v.first.first
     end
 
-    municipio = CGI.unescapeHTML(municipio) # para el apóstrofo catalán
+    municipio = CGI.unescapeHTML(municipio.strip) # para el apóstrofo catalán
 
     nombre = nombre.strip.gsub("  ", " ")
     nombre = CGI.unescapeHTML(nombre)
@@ -76,43 +73,97 @@ def playa(cod, historico)
     submuestreos.shift
 
     submuestreos.each_index do |i|
+      raise NayadeError, "Faltan coordenadas." if x[i].nil?
+      
       arrayout = [comunidad, provincia, municipio, nombre,
         pm[i][0], "nayade.rb", x[i][0], y[i][0], huso[i][0]]
 
       # expresión regular para obtener todas las mediciones
-      toma = Regexp.new('<' + '.+\s+.+valorCampoI">(.+)<' * 4)
+      toma = Regexp.new('valorCampoI">(.+)<.+\s+.+' * 4)
       mediciones = submuestreos[i].scan(toma)
 
-      if mediciones.empty?
-        $stderr.puts ": Playa sin muestreos."
-        return
-      end
+      raise NayadeError, "Playa sin muestreos." if mediciones.empty?
 
-      $stderr.print "\b"*4 # borrar cod actual de stderr
+      ultimas << (arrayout + mediciones[0]).to_csv # última medición
 
-      puts (arrayout + mediciones[0]).to_csv # última medición
-
-      if historico then
-        mediciones[1..-1].each do |m| # resto de mediciones
-          puts (arrayout + m).to_csv
-        end
+      mediciones.each do |m| # todas las mediciones
+        todas << (arrayout + m).to_csv
       end
     end
 
-  rescue StandardError
-    $stderr.puts ": Aquí no hay playa, vaya vaya." # hay códigos sin asignar a zonas de baño
-  rescue Interrupt
-    exit
+  rescue NayadeError => e
+    log = "#{cod}: #{e.message}\n"
   end
+
+  return [ultimas, todas, log]
 end
 
-todas = ARGV.delete("--todas") != nil
-ultimas = ARGV.delete("--ultimas") != nil
+def procesar_playas(inicio, fin)
+  cad_ult = ""
+  cad_hist = ""
+  cad_log = ""
+
+  (inicio..fin).each do |cod|
+    res = playa(cod)
+    cad_ult << res[0]
+    cad_hist << res[1]
+    cad_log << res[2]
+
+    Thread.exclusive { @count+=1 }
+  end
+
+  return [cad_ult, cad_hist, cad_log]
+end
+
+# Leer configuración
+Dir.chdir(File.dirname(__FILE__))
+config = YAML.load_file("config.yml")
+file_ult = config["ultimas"]
+file_hist = config["historico"]
+file_log = config["log"]
+nthreads = config["nthreads"]
+
+# Argumentos de entrada (utilizados para testeo)
 first = ARGV[0] ? ARGV[0].to_i : 1
 last = ARGV[1] ? ARGV[1].to_i : MAX_PLAYAS
 
+# Cabecera del CSV
 cabecera = %w(Comunidad Provincia Municipio Nombre punto_muestreo adoptada_por
-  utm_x utm_y utm_huso fecha_toma escherichia_coli enterococo observaciones)
-puts cabecera.to_csv if first==1
+  utm_x utm_y utm_huso fecha_toma escherichia_coli enterococo observaciones).to_csv
 
-(first..last).each { |cod| playa(cod, todas) }
+# Procesar las playas en paralelo
+total = last-first+1
+slice = total/nthreads # cantidad a procesar en cada thread
+threads = []
+array_ult = Array.new(nthreads) # datos de ultimas mediciones
+array_hist = Array.new(nthreads) # historico de todas las mediciones
+array_log = Array.new(nthreads) # log
+
+nthreads.times do |t| # Lanzar n threads
+  x = first + slice*t # inicio del bloque
+  y = (t+1==nthreads) ? last : first + slice*(t+1) - 1 # fin del floque
+
+  threads << Thread.new do
+    array_ult[t], array_hist[t], array_log[t] = procesar_playas(x, y)
+  end
+end
+
+# Monitorizar porcentaje
+threads << Thread.new do
+  loop do
+    porcentaje = @count*100/total
+    msg = "Procesadas #{@count} de #{total} playas... #{porcentaje}%"
+    print msg + "\b"*msg.size
+    break if @count==total
+    sleep 1
+  end
+  puts
+end
+
+# Esperar que todos los threads acaben
+threads.each(&:join)
+
+# Escribir resultado en los ficheros
+File.write(file_ult, cabecera + array_ult.join)
+File.write(file_hist, cabecera + array_hist.join)
+File.write(file_log, array_log.join)
